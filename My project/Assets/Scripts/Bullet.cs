2dcf19aad5c391dfdcb661c8c8004228cf2c58c7
reinickee <reinickedasilva@gmail.com>
2024-10-16 09:32:58 -0300

    testando

--- My project/Assets/Scripts/Bullet.cs
+++ My project/Assets/Scripts/Bullet.cs
@@ -9,6 +9,7 @@ public class Bullet : MonoBehaviourPun, IWeapon
     public float reloadTime = 1f;     // Tempo de recarga entre disparos
     private float nextFireTime = 0f;  // Controla o tempo até o próximo disparo
     public float bulletSpeed = 10f;   // Velocidade da bala
+    public float damageAmount = 20f;  // Dano causado pela bala
 
     void Start()
     {
@@ -30,10 +31,25 @@ public class Bullet : MonoBehaviourPun, IWeapon
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        // Lógica de colisão e destruição
+        // Verifica se o objeto colidido é um tanque
+        if (collision.gameObject.CompareTag(bulletPrefab.name))
+        {
+            // Aplica dano ao tanque atingido via RPC
+            if (photonView.IsMine)
+            {
+                TankHealth tankHealth = collision.gameObject.GetComponent<TankHealth>();
+                if (tankHealth != null)
+                {
+                    // Aplica dano ao tanque via RPC
+                    tankHealth.TakeDamage(damageAmount);
+                }
+            }
+        }
+
+        // Destroi a bala na rede após a colisão
         if (photonView.IsMine)
         {
-            PhotonNetwork.Destroy(gameObject); // Destroi a bala na rede
+            PhotonNetwork.Destroy(gameObject);
         }
     }
 
